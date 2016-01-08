7e5e7117ec41ba2abebc1eef2f8517d2d2f78030
Mike Penz <mikepenz@gmail.com>
2016-01-08 20:44:50 +0100

    * type adapters

--- app/src/main/java/com/mikepenz/fastadapter/app/SampleActivity.java
+++ app/src/main/java/com/mikepenz/fastadapter/app/SampleActivity.java
@@ -34,7 +34,7 @@ public class SampleActivity extends AppCompatActivity {
     //save our header or result
     private Drawer result = null;
     //save our FastAdapter
-    private FastAdapter fastAdapter;
+    private FastAdapter<SampleItem> fastAdapter;
 
     private UndoHelper undoHelper;
 
@@ -96,10 +96,10 @@ public class SampleActivity extends AppCompatActivity {
                 .build();
 
         //create our FastAdapter which will manage everything
-        fastAdapter = new FastAdapter();
+        fastAdapter = new FastAdapter<>();
 
         //create our ItemAdapter which will host our items
-        final ItemAdapter itemAdapter = new ItemAdapter();
+        final ItemAdapter<SampleItem> itemAdapter = new ItemAdapter<>();
 
         //
         undoHelper = new UndoHelper(itemAdapter, new UndoHelper.UndoListener() {
@@ -135,7 +135,7 @@ public class SampleActivity extends AppCompatActivity {
         rv.setAdapter(itemAdapter.wrap(fastAdapter));
 
         //fill with some sample data
-        List<IItem> items = new ArrayList<>();
+        List<SampleItem> items = new ArrayList<>();
         for (int i = 1; i <= 100; i++) {
             items.add(new SampleItem().withName("Test " + i).withIdentifier(100 + i));
         }
