7e5dae941ee3a1f835073326f75c9afe7193ea89
Mike Penz <mikepenz@gmail.com>
2016-01-14 00:15:35 +0100

    * add new UIUtil method which allows to have a pressed StateListDrawable state  * also allow to animate the fade inbetween the states

--- library/src/main/java/com/mikepenz/fastadapter/utils/FastAdapterUIUtils.java
+++ library/src/main/java/com/mikepenz/fastadapter/utils/FastAdapterUIUtils.java
@@ -13,29 +13,54 @@ import android.util.TypedValue;
 public class FastAdapterUIUtils {
 
     /**
-     * helper to create a StateListDrawable for the drawer item background
+     * helper to get the system default selectable background inclusive an active state
      *
-     * @param selected_color
-     * @return
+     * @param ctx            the context
+     * @param selected_color the selected color
+     * @param animate        true if you want to fade over the states (only animates if >= Build.VERSION_CODES.HONEYCOMB)
+     * @return the StateListDrawable
      */
-    public static StateListDrawable getItemBackground(int selected_color) {
-        ColorDrawable clrActive = new ColorDrawable(selected_color);
+    public static StateListDrawable getSelectableBackground(Context ctx, int selected_color, boolean animate) {
         StateListDrawable states = new StateListDrawable();
+
+        ColorDrawable clrActive = new ColorDrawable(selected_color);
         states.addState(new int[]{android.R.attr.state_selected}, clrActive);
+
+        states.addState(new int[]{}, ContextCompat.getDrawable(ctx, getSelectableBackground(ctx)));
+        //if possible we enable animating across states
+        if (animate && Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            int duration = ctx.getResources().getInteger(android.R.integer.config_shortAnimTime);
+            states.setEnterFadeDuration(duration);
+            states.setExitFadeDuration(duration);
+        }
         return states;
     }
 
     /**
-     * helper to get the system default selectable background inclusive an active state
+     * helper to get the system default selectable background inclusive an active and pressed state
      *
-     * @param ctx
-     * @param selected_color
-     * @return
+     * @param ctx            the context
+     * @param selected_color the selected color
+     * @param pressed_alpha  0-255
+     * @param animate        true if you want to fade over the states (only animates if >= Build.VERSION_CODES.HONEYCOMB)
+     * @return the StateListDrawable
      */
-    public static StateListDrawable getSelectableBackground(Context ctx, int selected_color) {
-        StateListDrawable states = getItemBackground(selected_color);
-        states.addState(new int[]{}, ContextCompat.getDrawable(ctx, getSelectableBackground(ctx)));
+    public static StateListDrawable getSelectablePressedBackground(Context ctx, int selected_color, int pressed_alpha, boolean animate) {
+        StateListDrawable states = getSelectableBackground(ctx, selected_color, animate);
+        ColorDrawable clrPressed = new ColorDrawable(adjustAlpha(selected_color, pressed_alpha));
+        states.addState(new int[]{android.R.attr.state_pressed}, clrPressed);
         return states;
+    }
+
+    /**
+     * adjusts the alpha of a color
+     *
+     * @param color the color
+     * @param alpha the alpha value we want to set 0-255
+     * @return the adjusted color
+     */
+    public static int adjustAlpha(int color, int alpha) {
+        return (alpha << 24) | (color & 0x00ffffff);
     }
 
     /**
