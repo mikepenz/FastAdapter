50094722a8a7b5ff64163d0d8bedae10621d3552
Mike Penz <mikepenz@gmail.com>
2016-01-16 00:51:57 +0100

    * provide simple drag helper api inside the FastAdapter

---
+++ library/src/main/java/com/mikepenz/fastadapter/drag/ItemTouchCallback.java
@@ -0,0 +1,13 @@
+package com.mikepenz.fastadapter.drag;
+
+public interface ItemTouchCallback {
+
+    /**
+     * Called when an item has been dragged
+     *
+     * @param oldPosition start position
+     * @param newPosition end position
+     * @return true if moved otherwise false
+     */
+    boolean itemTouchOnMove(int oldPosition, int newPosition);
+}
---
+++ library/src/main/java/com/mikepenz/fastadapter/drag/SimpleDragCallback.java
@@ -0,0 +1,39 @@
+package com.mikepenz.fastadapter.drag;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+/**
+ * based on the sample from
+ * https://github.com/AleBarreto/DragRecyclerView
+ */
+public class SimpleDragCallback extends ItemTouchHelper.SimpleCallback {
+
+    //our callback
+    private ItemTouchCallback mCallbackItemTouch; // interface
+    private boolean mIsDragEnabled = true;
+
+    public SimpleDragCallback(ItemTouchCallback itemTouchCallback) {
+        super(ItemTouchHelper.UP | ItemTouchHelper.DOWN, 0);
+        this.mCallbackItemTouch = itemTouchCallback;
+    }
+
+    public void setIsDragEnabled(boolean mIsDragEnabled) {
+        this.mIsDragEnabled = mIsDragEnabled;
+    }
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return mIsDragEnabled;
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        return mCallbackItemTouch.itemTouchOnMove(viewHolder.getAdapterPosition(), target.getAdapterPosition()); // information to the interface
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+        // swiped disabled
+    }
+}
