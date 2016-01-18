3de35d7760ed81ad0e7fc5e0b389a2cdd23c7dd7
Mike Penz <mikepenz@gmail.com>
2016-01-18 21:56:23 +0100

    * require type for AbstractItem to extend the IItem to prevent using the wrong type here

--- library/src/main/java/com/mikepenz/fastadapter/items/AbstractItem.java
+++ library/src/main/java/com/mikepenz/fastadapter/items/AbstractItem.java
@@ -15,7 +15,7 @@ import java.lang.reflect.ParameterizedType;
  * Created by mikepenz on 14.07.15.
  * Implements the general methods of the IItem interface to speed up development.
  */
-public abstract class AbstractItem<T, VH extends RecyclerView.ViewHolder> implements IItem<T, VH> {
+public abstract class AbstractItem<Item extends IItem, VH extends RecyclerView.ViewHolder> implements IItem<Item, VH> {
     // the identifier for this item
     protected long mIdentifier = -1;
 
@@ -25,9 +25,9 @@ public abstract class AbstractItem<T, VH extends RecyclerView.ViewHolder> implem
      * @param identifier
      * @return
      */
-    public T withIdentifier(long identifier) {
+    public Item withIdentifier(long identifier) {
         this.mIdentifier = identifier;
-        return (T) this;
+        return (Item) this;
     }
 
     /**
@@ -47,9 +47,9 @@ public abstract class AbstractItem<T, VH extends RecyclerView.ViewHolder> implem
      * @param object
      * @return
      */
-    public T withTag(Object object) {
+    public Item withTag(Object object) {
         this.mTag = object;
-        return (T) this;
+        return (Item) this;
     }
 
     /**
@@ -69,9 +69,9 @@ public abstract class AbstractItem<T, VH extends RecyclerView.ViewHolder> implem
      * @param enabled true if this item is enabled
      * @return
      */
-    public T withEnabled(boolean enabled) {
+    public Item withEnabled(boolean enabled) {
         this.mEnabled = enabled;
-        return (T) this;
+        return (Item) this;
     }
 
     /**
@@ -92,9 +92,9 @@ public abstract class AbstractItem<T, VH extends RecyclerView.ViewHolder> implem
      * @return
      */
     @Override
-    public T withSetSelected(boolean selected) {
+    public Item withSetSelected(boolean selected) {
         this.mSelected = selected;
-        return (T) this;
+        return (Item) this;
     }
 
     /**
@@ -115,9 +115,9 @@ public abstract class AbstractItem<T, VH extends RecyclerView.ViewHolder> implem
      * @return
      */
     @Override
-    public T withSelectable(boolean selectable) {
+    public Item withSelectable(boolean selectable) {
         this.mSelectable = selectable;
-        return (T) this;
+        return (Item) this;
     }
 
     /**
