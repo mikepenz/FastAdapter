4107fe74d9f7818f342c65a02711d3d0f7667561
Rainer-Lang <Rainer-Lang@users.noreply.github.com>
2016-01-31 11:29:22 +0100

    GenericItemSample: added MaterialScrollBar (Nova Launcher style) with CustomIndicator

--- app/src/main/java/com/mikepenz/fastadapter/app/GenericItemActivity.java
+++ app/src/main/java/com/mikepenz/fastadapter/app/GenericItemActivity.java
@@ -1,6 +1,7 @@
 package com.mikepenz.fastadapter.app;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,12 +11,15 @@ import android.view.View;
 
 import com.mikepenz.fastadapter.FastAdapter;
 import com.mikepenz.fastadapter.adapters.GenericItemAdapter;
-import com.mikepenz.fastadapter.app.generic.IconModel;
+import com.mikepenz.fastadapter.app.adapter.FastScrollIndicatorAdapter;
 import com.mikepenz.fastadapter.app.generic.GenericIconItem;
+import com.mikepenz.fastadapter.app.generic.IconModel;
 import com.mikepenz.iconics.Iconics;
 import com.mikepenz.iconics.typeface.ITypeface;
 import com.mikepenz.itemanimators.SlideDownAlphaAnimator;
 import com.mikepenz.materialize.MaterializeBuilder;
+import com.turingtechnologies.materialscrollbar.CustomIndicator;
+import com.turingtechnologies.materialscrollbar.DragScrollBar;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -54,55 +58,13 @@ public class GenericItemActivity extends AppCompatActivity {
 
         GenericItemAdapter<IconModel, GenericIconItem> itemAdapter = new GenericItemAdapter<>(GenericIconItem.class, IconModel.class);
 
+        final FastScrollIndicatorAdapter<GenericIconItem> fastScrollIndicatorAdapter = new FastScrollIndicatorAdapter<>();
+        rv.setAdapter(fastScrollIndicatorAdapter.wrap(itemAdapter.wrap(fastAdapter)));
+
+        DragScrollBar materialScrollBar = new DragScrollBar(this, rv, true);
+        materialScrollBar.setHandleColour(ContextCompat.getColor(this, R.color.colorAccent));
+        materialScrollBar.setHandleOffColour(ContextCompat.getColor(this, R.color.colorAccent));
+        materialScrollBar.addIndicator(new CustomIndicator(this), true);
+
         rv.setLayoutManager(gridLayoutManager);
         rv.setItemAnimator(new SlideDownAlphaAnimator());
-        rv.setAdapter(itemAdapter.wrap(fastAdapter));
-
-        //order fonts by their name
-        List<ITypeface> mFonts = new ArrayList<>(Iconics.getRegisteredFonts(this));
-        Collections.sort(mFonts, new Comparator<ITypeface>() {
-            @Override
-            public int compare(final ITypeface object1, final ITypeface object2) {
-                return object1.getFontName().compareTo(object2.getFontName());
-            }
-        });
-
-        //add all icons of all registered Fonts to the list
-        ArrayList<IconModel> models = new ArrayList<>();
-        for (ITypeface font : mFonts) {
-            for (String icon : font.getIcons()) {
-                models.add(new IconModel(font.getIcon(icon)));
-            }
-        }
-
-        //fill with some sample data
-        itemAdapter.addModel(models);
-
-        //restore selections (this has to be done after the items were added
-        fastAdapter.withSavedInstanceState(savedInstanceState);
-
-        //set the back arrow in the toolbar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(false);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        //add the values which need to be saved from the adapter to the bundel
-        outState = fastAdapter.saveInstanceState(outState);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        //handle the click on the back arrow click
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-}
