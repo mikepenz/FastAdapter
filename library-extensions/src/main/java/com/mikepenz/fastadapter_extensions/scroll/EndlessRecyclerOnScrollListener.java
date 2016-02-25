32976c3efe923d83ff288c3681ef5d27828a949e
fabianterhorst <fabian.terhorst@gmail.com>
2016-02-25 14:07:48 +0100

    add EndlessRecyclerOnScrollListener

---
+++ library-extensions/src/main/java/com/mikepenz/fastadapter_extensions/scroll/EndlessRecyclerOnScrollListener.java
@@ -0,0 +1,68 @@
+package com.mikepenz.fastadapter_extensions.scroll;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScrollListener {
+
+    private int mPreviousTotal = 0;
+    private boolean mLoading = true;
+    private int mVisibleThreshold = 30;
+    private int mFirstVisibleItem, mVisibleItemCount, mTotalItemCount;
+
+    private int mCurrentPage = 1;
+
+    private LinearLayoutManager mLinearLayoutManager;
+
+    public EndlessRecyclerOnScrollListener(LinearLayoutManager linearLayoutManager) {
+        this.mLinearLayoutManager = linearLayoutManager;
+    }
+
+    public EndlessRecyclerOnScrollListener(LinearLayoutManager linearLayoutManager, int visibleThreshold) {
+        this.mLinearLayoutManager = linearLayoutManager;
+        this.mVisibleThreshold = visibleThreshold;
+    }
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+
+        mVisibleItemCount = recyclerView.getChildCount();
+        mTotalItemCount = mLinearLayoutManager.getItemCount();
+        mFirstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
+
+        if (mLoading) {
+            if (mTotalItemCount > mPreviousTotal) {
+                mLoading = false;
+                mPreviousTotal = mTotalItemCount;
+            }
+        }
+        if (!mLoading && (mTotalItemCount - mVisibleItemCount)
+                <= (mFirstVisibleItem + mVisibleThreshold)) {
+
+            mCurrentPage++;
+
+            onLoadMore(mCurrentPage);
+
+            mLoading = true;
+        }
+    }
+
+    public int getTotalItemCount() {
+        return mTotalItemCount;
+    }
+
+    public int getFirstVisibleItem() {
+        return mFirstVisibleItem;
+    }
+
+    public int getVisibleItemCount() {
+        return mVisibleItemCount;
+    }
+
+    public int getCurrentPage() {
+        return mCurrentPage;
+    }
+
+    public abstract void onLoadMore(int currentPage);
+}
