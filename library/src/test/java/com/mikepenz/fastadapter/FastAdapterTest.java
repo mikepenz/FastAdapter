af0b248a4aa45303d63318af02a0456fb6b304b8
Shubham Chaudhary <me@shubhamchaudhary.in>
2016-03-18 00:23:13 +0530

    More tests

--- library/src/test/java/com/mikepenz/fastadapter/FastAdapterTest.java
+++ library/src/test/java/com/mikepenz/fastadapter/FastAdapterTest.java
@@ -1,10 +1,13 @@
 package com.mikepenz.fastadapter;
 
+import android.support.v7.widget.RecyclerView;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 
 /**
@@ -12,8 +15,32 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @RunWith(RobolectricGradleTestRunner.class)
 public class FastAdapterTest {
+    private FastAdapter<IItem> adapter;
+
+    @Before public void setUp() throws Exception {
+        adapter = new FastAdapter<>();
+    }
+
     @Test public void constructor_HasStableId() throws Exception {
-        FastAdapter<IItem> adapter = new FastAdapter<>();
         assertThat(adapter.hasStableIds()).isTrue();
+    }
+
+    @Test public void constructor_Default_IsSelectable() throws Exception {
+        assertThat(adapter.isSelectable()).isTrue();    //default
+    }
+
+    @Test public void withSelectable() throws Exception {
+        assertThat(adapter.withSelectable(true).isSelectable()).isTrue();
+        assertThat(adapter.withSelectable(false).isSelectable()).isFalse();
+    }
+
+    @Test public void withBindViewHolderListener_OnBindViewHolder_Callback() throws Exception {
+        FastAdapter.OnBindViewHolderListener listener = mock(FastAdapter.OnBindViewHolderListener.class);
+        RecyclerView.ViewHolder holder = mock(RecyclerView.ViewHolder.class);
+        adapter.withOnBindViewHolderListener(listener);
+
+        adapter.onBindViewHolder(holder, 10);
+
+        verify(listener, only()).onBindViewHolder(holder, 10);
     }
 }
