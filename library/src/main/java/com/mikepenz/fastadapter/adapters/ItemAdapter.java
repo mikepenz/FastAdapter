34fff05340e720df569ec87a56c492ea38437f8b
Mike Penz <mikepenz@gmail.com>
2016-03-22 17:51:01 +0100

    * fix move function

--- library/src/main/java/com/mikepenz/fastadapter/adapters/ItemAdapter.java
+++ library/src/main/java/com/mikepenz/fastadapter/adapters/ItemAdapter.java
@@ -375,7 +375,7 @@ public class ItemAdapter<Item extends IItem> extends AbstractAdapter<Item> imple
         int preItemCount = getFastAdapter().getPreItemCount(fromPosition);
         Item item = mItems.get(fromPosition - preItemCount);
         mItems.remove(fromPosition - preItemCount);
-        mItems.set(toPosition - preItemCount, item);
+        mItems.add(toPosition - preItemCount, item);
         getFastAdapter().notifyAdapterItemMoved(fromPosition, toPosition);
         return this;
     }
