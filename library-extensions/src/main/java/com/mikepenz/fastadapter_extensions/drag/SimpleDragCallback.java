fe8886fb21590daff1eca30ae1f9d536d48f8f9f
fabianterhorst <fabian.terhorst@gmail.com>
2016-03-23 18:17:49 +0100

    update SimpleDragCallback
    make direction configurable

--- library-extensions/src/main/java/com/mikepenz/fastadapter_extensions/drag/SimpleDragCallback.java
+++ library-extensions/src/main/java/com/mikepenz/fastadapter_extensions/drag/SimpleDragCallback.java
@@ -1,7 +1,13 @@
 package com.mikepenz.fastadapter_extensions.drag;
 
+import android.support.annotation.IntDef;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+
+import com.mikepenz.fastadapter.IDraggable;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * based on the sample from
@@ -10,11 +16,27 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 public class SimpleDragCallback extends ItemTouchHelper.SimpleCallback {
 
     //our callback
-    private ItemTouchCallback mCallbackItemTouch; // interface
+    private final ItemTouchCallback mCallbackItemTouch; // interface
     private boolean mIsDragEnabled = true;
 
+    private int mDirections = UP_DOWN;
+
+    public static final int ALL = ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+    public static final int UP_DOWN = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+
+    @IntDef({ALL, UP_DOWN})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Directions {
+    }
+
+    public SimpleDragCallback(@Directions int directions, ItemTouchCallback itemTouchCallback) {
+        super(directions, 0);
+        this.mDirections = directions;
+        this.mCallbackItemTouch = itemTouchCallback;
+    }
+
     public SimpleDragCallback(ItemTouchCallback itemTouchCallback) {
-        super(ItemTouchHelper.UP | ItemTouchHelper.DOWN, 0);
+        super(UP_DOWN, 0);
         this.mCallbackItemTouch = itemTouchCallback;
     }
 
@@ -30,6 +52,19 @@ public class SimpleDragCallback extends ItemTouchHelper.SimpleCallback {
     @Override
     public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
         return mCallbackItemTouch.itemTouchOnMove(viewHolder.getAdapterPosition(), target.getAdapterPosition()); // information to the interface
+    }
+
+    @Override
+    public int getDragDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        if (viewHolder.itemView.getTag() instanceof IDraggable) {
+            if (((IDraggable) viewHolder.itemView.getTag()).isDraggable()) {
+                return super.getDragDirs(recyclerView, viewHolder);
+            } else {
+                return 0;
+            }
+        } else {
+            return mDirections;
+        }
     }
 
     @Override
