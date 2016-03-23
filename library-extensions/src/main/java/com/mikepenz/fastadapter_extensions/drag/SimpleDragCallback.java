41a77e6d6609739a9b2b5c7f320baf54688dc0c0
fabianterhorst <fabian.terhorst@gmail.com>
2016-03-23 18:32:45 +0100

    update SimpleDragCallback
    make directions configurable

--- library-extensions/src/main/java/com/mikepenz/fastadapter_extensions/drag/SimpleDragCallback.java
+++ library-extensions/src/main/java/com/mikepenz/fastadapter_extensions/drag/SimpleDragCallback.java
@@ -15,8 +15,24 @@ public class SimpleDragCallback extends ItemTouchHelper.SimpleCallback {
     private final ItemTouchCallback mCallbackItemTouch; // interface
     private boolean mIsDragEnabled = true;
 
+    private int mDirections = UP_DOWN;
+
+    public static final int ALL = ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+    public static final int UP_DOWN = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+
+    @IntDef({ALL, UP_DOWN})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Directions {
+    }
+
+    public SimpleDragCallback(@Directions int directions, ItemTouchCallback itemTouchCallback) {
+        super(directions, 0);
+        this.mDirections = directions;
+        this.mCallbackItemTouch = itemTouchCallback;
+    }
+
     public SimpleDragCallback(ItemTouchCallback itemTouchCallback) {
-        super(ItemTouchHelper.UP | ItemTouchHelper.DOWN, 0);
+        super(UP_DOWN, 0);
         this.mCallbackItemTouch = itemTouchCallback;
     }
 
@@ -43,7 +59,7 @@ public class SimpleDragCallback extends ItemTouchHelper.SimpleCallback {
                 return 0;
             }
         } else {
-            return ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+            return mDirections;
         }
     }
 
