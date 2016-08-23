288b8206bd8155fb7cada2c83e0c3b2a792a9781
Mike Penz <mikepenz@gmail.com>
2016-08-23 21:05:27 +0200

    * initial code to support the DiffUtils with the FastAdapter

--- library/src/main/java/com/mikepenz/fastadapter/adapters/ItemAdapter.java
+++ library/src/main/java/com/mikepenz/fastadapter/adapters/ItemAdapter.java
@@ -1,5 +1,9 @@
 package com.mikepenz.fastadapter.adapters;
 
+import android.support.annotation.Nullable;
+import android.support.v7.util.DiffUtil;
+import android.support.v7.util.ListUpdateCallback;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Filter;
 
 import com.mikepenz.fastadapter.AbstractAdapter;
@@ -225,6 +229,95 @@ public class ItemAdapter<Item extends IItem> extends AbstractAdapter<Item> imple
             IdDistributor.checkIds(subItems);
         }
         return collapsible.withSubItems(subItems);
+    }
+
+    /**
+     * this sets a new list of items using the DiffCallback to calculate the difference of the two lists (uses the DiffUtils)
+     *
+     * @param items       the list of the new items
+     * @param callback    the callback to check if items are different or equal
+     * @param detectMoves is a bit slower as it will also check if items were moved
+     * @return this
+     */
+    public ItemAdapter<Item> set(final List<Item> items, final DiffCallback<Item> callback, boolean detectMoves) {
+        if (mUseIdDistributor) {
+            IdDistributor.checkIds(items);
+        }
+
+        //first collapse all items
+        getFastAdapter().collapse(false);
+
+        //make sure the new items list is not a reference of the already mItems list
+        if (items != mItems) {
+            //remove all previous items
+            if (!mItems.isEmpty()) {
+                mItems.clear();
+            }
+
+            //add all new items to the list
+            mItems.addAll(items);
+        }
+
+        //map the types
+        mapPossibleTypes(items);
+
+        //if we have a comparator then sort
+        if (mComparator != null) {
+            Collections.sort(mItems, mComparator);
+        }
+
+        DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffUtil.Callback() {
+            @Override
+            public int getOldListSize() {
+                return mItems.size();
+            }
+
+            @Override
+            public int getNewListSize() {
+                return items.size();
+            }
+
+            @Override
+            public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+                return callback.areItemsTheSame(mItems.get(oldItemPosition), items.get(newItemPosition));
+            }
+
+            @Override
+            public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+                return callback.areContentsTheSame(mItems.get(oldItemPosition), items.get(newItemPosition));
+            }
+
+            @Nullable
+            @Override
+            public Object getChangePayload(int oldItemPosition, int newItemPosition) {
+                Object result = callback.getChangePayload(mItems.get(oldItemPosition), oldItemPosition, items.get(newItemPosition), newItemPosition);
+                return result == null ? super.getChangePayload(oldItemPosition, newItemPosition) : result;
+            }
+        }, detectMoves);
+
+        result.dispatchUpdatesTo(new ListUpdateCallback() {
+            @Override
+            public void onInserted(int position, int count) {
+                getFastAdapter().notifyAdapterItemRangeInserted(position, count);
+            }
+
+            @Override
+            public void onRemoved(int position, int count) {
+                getFastAdapter().notifyAdapterItemRangeRemoved(position, count);
+            }
+
+            @Override
+            public void onMoved(int fromPosition, int toPosition) {
+                getFastAdapter().notifyAdapterItemMoved(fromPosition, toPosition);
+            }
+
+            @Override
+            public void onChanged(int position, int count, Object payload) {
+                getFastAdapter().notifyAdapterItemRangeChanged(position, count, payload);
+            }
+        });
+
+        return this;
     }
 
     /**
@@ -551,5 +644,54 @@ public class ItemAdapter<Item extends IItem> extends AbstractAdapter<Item> imple
                 return getFastAdapter().getSelectedItems();
             }
         }
+    }
+
+    /**
+     * DiffCallback used to efficiently update the items inside the list with the new DiffUtils
+     */
+    public interface DiffCallback<Item> {
+        /**
+         * Called by the DiffUtil to decide whether two object represent the same Item.
+         * For example, if your items have unique ids, this method should check their id equality.
+         *
+         * @param oldItem the item in the old list
+         * @param newItem the item in the new list
+         * @return True if the two items represent the same object or false if they are different.
+         */
+        boolean areItemsTheSame(Item oldItem, Item newItem);
+
+        /**
+         * Called by the DiffUtil when it wants to check whether two items have the same data.
+         * DiffUtil uses this information to detect if the contents of an item has changed.
+         * DiffUtil uses this method to check equality instead of {@link Object#equals(Object)}
+         * so that you can change its behavior depending on your UI.
+         * For example, if you are using DiffUtil with a
+         * {@link android.support.v7.widget.RecyclerView.Adapter RecyclerView.Adapter}, you should
+         * return whether the items' visual representations are the same.
+         * This method is called only if #areItemsTheSame(Item, Item) returns
+         * {@code true} for these items.
+         *
+         * @param oldItem the item in the old list
+         * @param newItem the item in the new list
+         * @return True if the contents of the items are the same or false if they are different.
+         */
+        boolean areContentsTheSame(Item oldItem, Item newItem);
+
+        /**
+         * When {#areItemsTheSame(Item, Item)} returns {@code true} for two items and
+         * {#areContentsTheSame(Item, Item)} returns false for them, DiffUtil
+         * calls this method to get a payload about the change.
+         * For example, if you are using DiffUtil with {@link RecyclerView}, you can return the
+         * particular field that changed in the item and your
+         * {@link android.support.v7.widget.RecyclerView.ItemAnimator ItemAnimator} can use that
+         * information to run the correct animation.
+         * Default implementation returns {@code null}.
+         *
+         * @param oldItemPosition The position of the item in the old list
+         * @param newItemPosition The position of the item in the new list
+         * @return A payload object that represents the change between the two items.
+         */
+        @Nullable
+        Object getChangePayload(Item oldItem, int oldItemPosition, Item newItem, int newItemPosition);
     }
 }
