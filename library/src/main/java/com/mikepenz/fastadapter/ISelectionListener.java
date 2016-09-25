8b1d19ffa57a11d002141a3fc2b4b7b74968bd22
MFlisar <mflisar.development@gmail.com>
2016-09-25 12:26:56 +0200

    generic ISelectionListener

--- library/src/main/java/com/mikepenz/fastadapter/ISelectionListener.java
+++ library/src/main/java/com/mikepenz/fastadapter/ISelectionListener.java
@@ -4,12 +4,12 @@ package com.mikepenz.fastadapter;
  * Created by flisar on 21.09.2016.
  */
 
-public interface ISelectionListener {
+public interface ISelectionListener<Item extends IItem> {
     /**
      * is called, whenever the provided item is selected or deselected
      *
      * @param item the item who's selection state changed
      *             param selected the new selection state of the item
      */
-    void onSelectionChanged(IItem item, boolean selected);
+    void onSelectionChanged(Item item, boolean selected);
 }
