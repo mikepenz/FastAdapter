ceb3c07b36caa76978d7cdf24bbd472d35ccb2c3
fabianterhorst <fabian.terhorst@gmail.com>
2016-12-19 17:59:00 +0100

    * improve benchmarks

--- library/src/androidTest/java/com/mikepenz/fastadapter/FastAdapterBenchmark.java
+++ library/src/androidTest/java/com/mikepenz/fastadapter/FastAdapterBenchmark.java
@@ -1,12 +1,19 @@
 package com.mikepenz.fastadapter;
 
-import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter;
+import android.support.test.InstrumentationRegistry;
+
+import com.mikepenz.fastadapter.adapters.ItemAdapter;
 
 import org.junit.runner.RunWith;
+
+import java.io.File;
 
 import dk.ilios.spanner.AfterExperiment;
 import dk.ilios.spanner.BeforeExperiment;
 import dk.ilios.spanner.Benchmark;
+import dk.ilios.spanner.BenchmarkConfiguration;
+import dk.ilios.spanner.SpannerConfig;
+import dk.ilios.spanner.config.RuntimeInstrumentConfig;
 import dk.ilios.spanner.junit.SpannerRunner;
 
 /**
@@ -16,11 +23,25 @@ import dk.ilios.spanner.junit.SpannerRunner;
 @RunWith(SpannerRunner.class)
 public class FastAdapterBenchmark {
 
-    private FastItemAdapter<TestItem> fastItemAdapter;
+    private File filesDir = InstrumentationRegistry.getTargetContext().getFilesDir();
+    private File resultsDir = new File(filesDir, "results");
+
+    @BenchmarkConfiguration
+    public SpannerConfig configuration = new SpannerConfig.Builder()
+            .saveResults(resultsDir, FastAdapterBenchmark.class.getCanonicalName() + ".json") // Save results to disk
+            .medianFailureLimit(Float.MAX_VALUE) // Fail if difference vs. baseline is to big. Should normally be 10-15%  (0.15)
+            .addInstrument(RuntimeInstrumentConfig.defaultConfig()) // Configure how benchmark is run/measured
+            .maxBenchmarkThreads(1)
+            .build();
+
+    private ItemAdapter<TestItem> itemAdapter;
+    private FastAdapter<TestItem> fastAdapter;
 
     @BeforeExperiment
     public void before() {
-        fastItemAdapter = new FastItemAdapter<>();
+        itemAdapter = new ItemAdapter<>();
+        fastAdapter = new FastAdapter<>();
+        itemAdapter.wrap(fastAdapter);
     }
 
     @AfterExperiment
@@ -30,6 +51,6 @@ public class FastAdapterBenchmark {
 
     @Benchmark
     public void addItems() {
-        fastItemAdapter.items().setNewList(TestDataGenerator.genTestItemList(100));
+        itemAdapter.setNewList(TestDataGenerator.genTestItemList(1000));
     }
 }
