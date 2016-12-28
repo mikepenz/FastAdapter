9894cf973a08bfc69c22c0387e62e95e20a743a7
fabianterhorst <fabian.terhorst@gmail.com>
2016-12-28 09:51:49 +0100

    * issues aren´t used currently

--- library/src/test/java/com/mikepenz/fastadapter/Issues.java
+++ library/src/test/java/com/mikepenz/fastadapter/Issues.java
@@ -1,13 +1,10 @@
 package com.mikepenz.fastadapter;
 
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-
 /**
  * Created by fabianterhorst on 22.12.16.
  */
 
-@RunWith(RobolectricTestRunner.class)
+//@RunWith(RobolectricTestRunner.class)
 public class Issues {
 
 }
