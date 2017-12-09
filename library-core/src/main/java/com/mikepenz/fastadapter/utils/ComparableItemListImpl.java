652b8f70e362009aa724982c07450d8126fffe65
Mike Penz <mikepenz@gmail.com>
2017-12-09 13:42:49 +0100

    * do not duplicate code in `CompareableItemListImpl`

--- library-core/src/main/java/com/mikepenz/fastadapter/utils/ComparableItemListImpl.java
+++ library-core/src/main/java/com/mikepenz/fastadapter/utils/ComparableItemListImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
  * The default item list implementation
  */
 
-public class ComparableItemListImpl<Item extends IItem> extends DefaultItemList<Item> {
+public class ComparableItemListImpl<Item extends IItem> extends DefaultItemListImpl<Item> {
 
     private List<Item> mItems;
 
@@ -28,74 +28,12 @@ public class ComparableItemListImpl<Item extends IItem> extends DefaultItemList<
     }
 
     @Override
-    public Item get(int position) {
-        return mItems.get(position);
-    }
-
-    @Override
-    public List<Item> getItems() {
-        return mItems;
-    }
-
-    @Override
-    public int getAdapterPosition(long identifier) {
-        for (int i = 0, size = mItems.size(); i < size; i++) {
-            if (mItems.get(i).getIdentifier() == identifier) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    @Override
-    public void remove(int position, int preItemCount) {
-        mItems.remove(position - preItemCount);
-        getFastAdapter().notifyAdapterItemRemoved(position);
-    }
-
-    @Override
-    public void removeRange(int position, int itemCount, int preItemCount) {
-        //global position to relative
-        int length = mItems.size();
-        //make sure we do not delete to many items
-        int saveItemCount = Math.min(itemCount, length - position + preItemCount);
-
-        for (int i = 0; i < saveItemCount; i++) {
-            mItems.remove(position - preItemCount);
-        }
-        getFastAdapter().notifyAdapterItemRangeRemoved(position, saveItemCount);
-    }
-
-    @Override
     public void move(int fromPosition, int toPosition, int preItemCount) {
         Item item = mItems.get(fromPosition - preItemCount);
         mItems.remove(fromPosition - preItemCount);
         mItems.add(toPosition - preItemCount, item);
         Collections.sort(mItems, mComparator);
         getFastAdapter().notifyAdapterDataSetChanged();
-    }
-
-    @Override
-    public int size() {
-        return mItems.size();
-    }
-
-    @Override
-    public void clear(int preItemCount) {
-        int size = mItems.size();
-        mItems.clear();
-        getFastAdapter().notifyAdapterItemRangeRemoved(preItemCount, size);
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return mItems.isEmpty();
-    }
-
-    @Override
-    public void set(int position, Item item) {
-        mItems.set(position, item);
-        getFastAdapter().notifyAdapterItemInserted(position);
     }
 
     @Override
@@ -109,12 +47,6 @@ public class ComparableItemListImpl<Item extends IItem> extends DefaultItemList<
     public void addAll(int position, List<Item> items, int preItemCount) {
         mItems.addAll(position - preItemCount, items);
         Collections.sort(items, mComparator);
-        getFastAdapter().notifyAdapterDataSetChanged();
-    }
-
-    @Override
-    public void setNewList(List<Item> items) {
-        mItems = new ArrayList<>(items);
         getFastAdapter().notifyAdapterDataSetChanged();
     }
 }
