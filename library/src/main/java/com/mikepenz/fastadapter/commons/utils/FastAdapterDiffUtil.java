8328f5c89ee9e004252f413551967f96f180a12c
Mike Penz <mikepenz@gmail.com>
2018-06-02 19:11:23 +0200

    * add javadoc for `FastAdapterDiffUtl`. FIX #678

--- library/src/main/java/com/mikepenz/fastadapter/commons/utils/FastAdapterDiffUtil.java
+++ library/src/main/java/com/mikepenz/fastadapter/commons/utils/FastAdapterDiffUtil.java
@@ -22,6 +22,24 @@ import java.util.List;
 
 public class FastAdapterDiffUtil {
 
+    /**
+     * This method will compute a {@link android.support.v7.util.DiffUtil.DiffResult} based on the given adapter, and the list of new items.
+     * <p>
+     * It automatically collapses all expandables (if enabled) as they are not supported by the diff util,
+     * pre sort the items based on the comparator if available,
+     * map the new item types for the FastAdapter then calculates the {@link android.support.v7.util.DiffUtil.DiffResult} using the {@link DiffUtil}.
+     * <p>
+     * As the last step it will replace the items inside the adapter with the new set of items provided.
+     *
+     * @param adapter     the adapter containing the current items.
+     * @param items       the new set of items we want to put into the adapter
+     * @param callback    the callback used to implement the required checks to identify changes of items.
+     * @param detectMoves configuration for the {@link DiffUtil#calculateDiff(DiffUtil.Callback, boolean)} method
+     * @param <A>         The adapter type, whereas A extends {@link ModelAdapter}
+     * @param <Model>     The model type we work with
+     * @param <Item>      The item type kept in the adapter
+     * @return the {@link android.support.v7.util.DiffUtil.DiffResult} computed.
+     */
     public static <A extends ModelAdapter<Model, Item>, Model, Item extends IItem> DiffUtil.DiffResult calculateDiff(final A adapter, final List<Item> items, final DiffCallback<Item> callback, final boolean detectMoves) {
         if (adapter.isUseIdDistributor()) {
             adapter.getIdDistributor().checkIds(items);
@@ -79,76 +97,171 @@ public class FastAdapterDiffUtil {
         }
     }
 
+    /**
+     * Dispatches a {@link android.support.v7.util.DiffUtil.DiffResult} to the given Adapter.
+     *
+     * @param adapter the adapter to dispatch the updates to
+     * @param result  the computed {@link android.support.v7.util.DiffUtil.DiffResult}
+     * @return the adapter to allow chaining
+     */
     public static <A extends ModelAdapter<Model, Item>, Model, Item extends IItem> A set(final A adapter, DiffUtil.DiffResult result) {
         result.dispatchUpdatesTo(new FastAdapterListUpdateCallback<>(adapter));
         return adapter;
     }
 
+    /**
+     * convenient function for {@link #calculateDiff(ModelAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the {@link android.support.v7.util.DiffUtil.DiffResult} computed.
+     */
     public static <A extends ModelAdapter<Model, Item>, Model, Item extends IItem> DiffUtil.DiffResult calculateDiff(final A adapter, final List<Item> items, final DiffCallback<Item> callback) {
         return calculateDiff(adapter, items, callback, true);
     }
 
+    /**
+     * convenient function for {@link #calculateDiff(ModelAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the {@link android.support.v7.util.DiffUtil.DiffResult} computed.
+     */
     public static <A extends ModelAdapter<Model, Item>, Model, Item extends IItem> DiffUtil.DiffResult calculateDiff(final A adapter, final List<Item> items, final boolean detectMoves) {
         return calculateDiff(adapter, items, new DiffCallbackImpl<Item>(), detectMoves);
     }
 
+    /**
+     * convenient function for {@link #calculateDiff(ModelAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the {@link android.support.v7.util.DiffUtil.DiffResult} computed.
+     */
     public static <A extends ModelAdapter<Model, Item>, Model, Item extends IItem> DiffUtil.DiffResult calculateDiff(final A adapter, final List<Item> items) {
         return calculateDiff(adapter, items, new DiffCallbackImpl<Item>(), true);
     }
 
+    /**
+     * Calculates a {@link android.support.v7.util.DiffUtil.DiffResult} given the adapter and the items, and will directly dispatch them to the adapter.
+     *
+     * @param adapter     the adapter containing the current items.
+     * @param items       the new set of items we want to put into the adapter
+     * @param callback    the callback used to implement the required checks to identify changes of items.
+     * @param detectMoves configuration for the {@link DiffUtil#calculateDiff(DiffUtil.Callback, boolean)} method
+     * @param <A>         The adapter type, whereas A extends {@link ModelAdapter}
+     * @param <Model>     The model type we work with
+     * @param <Item>      The item type kept in the adapter
+     * @return the adapter to allow chaining
+     */
     public static <A extends ModelAdapter<Model, Item>, Model, Item extends IItem> A set(final A adapter, final List<Item> items, final DiffCallback<Item> callback, final boolean detectMoves) {
         DiffUtil.DiffResult result = calculateDiff(adapter, items, callback, detectMoves);
         return set(adapter, result);
     }
 
+    /**
+     * convenient function for {@link #set(FastItemAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the adapter to allow chaining
+     */
     public static <A extends ModelAdapter<Model, Item>, Model, Item extends IItem> A set(final A adapter, final List<Item> items, final DiffCallback<Item> callback) {
         return set(adapter, items, callback, true);
     }
 
+    /**
+     * convenient function for {@link #set(FastItemAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the adapter to allow chaining
+     */
     public static <A extends ModelAdapter<Model, Item>, Model, Item extends IItem> A set(final A adapter, final List<Item> items, final boolean detectMoves) {
         return set(adapter, items, new DiffCallbackImpl<Item>(), detectMoves);
     }
 
+    /**
+     * convenient function for {@link #set(FastItemAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the adapter to allow chaining
+     */
     public static <A extends ModelAdapter<Model, Item>, Model, Item extends IItem> A set(final A adapter, final List<Item> items) {
         return set(adapter, items, new DiffCallbackImpl<Item>());
     }
 
+    /**
+     * convenient function for {@link #calculateDiff(ModelAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the {@link android.support.v7.util.DiffUtil.DiffResult} computed.
+     */
     public static <A extends FastItemAdapter<Item>, Item extends IItem> DiffUtil.DiffResult calculateDiff(final A adapter, final List<Item> items, final DiffCallback<Item> callback) {
         return calculateDiff(adapter.getItemAdapter(), items, callback);
     }
 
+    /**
+     * convenient function for {@link #calculateDiff(ModelAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the {@link android.support.v7.util.DiffUtil.DiffResult} computed.
+     */
     public static <A extends FastItemAdapter<Item>, Item extends IItem> DiffUtil.DiffResult calculateDiff(final A adapter, final List<Item> items, final boolean detectMoves) {
         return calculateDiff(adapter.getItemAdapter(), items, detectMoves);
     }
 
+    /**
+     * convenient function for {@link #calculateDiff(ModelAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the {@link android.support.v7.util.DiffUtil.DiffResult} computed.
+     */
     public static <A extends FastItemAdapter<Item>, Item extends IItem> DiffUtil.DiffResult calculateDiff(final A adapter, final List<Item> items) {
         return calculateDiff(adapter.getItemAdapter(), items);
     }
 
+    /**
+     * convenient function for {@link #set(FastItemAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the adapter to allow chaining
+     */
     public static <A extends FastItemAdapter<Item>, Item extends IItem> A set(final A adapter, final List<Item> items, final DiffCallback<Item> callback, final boolean detectMoves) {
         set(adapter.getItemAdapter(), items, callback, detectMoves);
         return adapter;
     }
 
+    /**
+     * convenient function for {@link #set(FastItemAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the adapter to allow chaining
+     */
     public static <A extends FastItemAdapter<Item>, Item extends IItem> A set(final A adapter, final List<Item> items, final DiffCallback<Item> callback) {
         set(adapter.getItemAdapter(), items, callback);
         return adapter;
     }
 
+    /**
+     * convenient function for {@link #set(FastItemAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the adapter to allow chaining
+     */
     public static <A extends FastItemAdapter<Item>, Item extends IItem> A set(final A adapter, final List<Item> items, final boolean detectMoves) {
         set(adapter.getItemAdapter(), items, detectMoves);
         return adapter;
     }
 
+    /**
+     * convenient function for {@link #set(FastItemAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the adapter to allow chaining
+     */
+
     public static <A extends FastItemAdapter<Item>, Item extends IItem> A set(final A adapter, final List<Item> items) {
         return set(adapter, items, new DiffCallbackImpl<Item>());
     }
 
+    /**
+     * convenient function for {@link #set(FastItemAdapter, List, DiffCallback, boolean)}
+     *
+     * @return the adapter to allow chaining
+     */
     public static <A extends FastItemAdapter<Item>, Item extends IItem> A set(final A adapter, DiffUtil.DiffResult result) {
         set(adapter.getItemAdapter(), result);
         return adapter;
     }
 
+    /**
+     * Convenient implementation for the {@link android.support.v7.util.DiffUtil.Callback} to simplify difference calculation using {@link FastAdapter} items.
+     *
+     * @param <Item> the item type in the adapter
+     */
     private static final class FastAdapterCallback<Item extends IItem> extends DiffUtil.Callback {
 
         private final List<Item> oldItems;
@@ -189,6 +302,9 @@ public class FastAdapterDiffUtil {
         }
     }
 
+    /**
+     * Default implementation of the {@link ListUpdateCallback} to apply changes to the adapter and notify about the changes.
+     */
     private static final class FastAdapterListUpdateCallback<A extends ModelAdapter<Model, Item>, Model, Item extends IItem> implements ListUpdateCallback {
 
         private final A adapter;
