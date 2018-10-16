9b31b53a6be98c6268bc88d010624d10427f313d
Fabian Terhorst <fabian.terhorst@gmail.com>
2018-10-16 19:37:51 +0200

    Add diff utils test

--- library/src/test/java/com/mikepenz/fastadapter/FastAdapterDiffUtilTest.java
+++ library/src/test/java/com/mikepenz/fastadapter/FastAdapterDiffUtilTest.java
@@ -3,13 +3,13 @@ package com.mikepenz.fastadapter;
 import com.mikepenz.fastadapter.adapters.ItemAdapter;
 import com.mikepenz.fastadapter.commons.utils.FastAdapterDiffUtil;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -66,6 +66,24 @@ public class FastAdapterDiffUtilTest {
         Collections.shuffle(updatedList, new Random(1342348L));
 
         test(originalList, updatedList);
+    }
+
+    @Test
+    public void testDiffUtilIdDistributor() {
+        TestItem testItem1 = new TestItem("Test1");
+        TestItem testItem2 = new TestItem("Test2");
+        List<TestItem> originalList = new ArrayList<>();
+        originalList.add(testItem1);
+        originalList.add(testItem2);
+
+        List<TestItem> updatedList = new ArrayList<>();
+        updatedList.add(testItem2);
+        updatedList.add(testItem1);
+
+        test(originalList, updatedList);
+
+        Assert.assertEquals(updatedList.get(0).getIdentifier(), originalList.get(1).getIdentifier());
+        Assert.assertEquals(updatedList.get(1).getIdentifier(), originalList.get(0).getIdentifier());
     }
 
     public void test(List<TestItem> original, List<TestItem> adjusted) {
