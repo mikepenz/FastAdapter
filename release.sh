888d0642301993f5961e8987f6e6a52c14e85a91
Mike Penz <mikepenz@gmail.com>
2018-11-24 19:47:22 +0100

    * release v4.0.0-alpha4

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@
 
 if [ "$1" = "release" ];
 then
-    ./gradlew library-core:bintrayUpload -Plibrary_extensions_core_only
+    ./gradlew library-core:bintrayUpload -Plibrary_core_only
     ./gradlew library-extensions-diff:bintrayUpload -x test -Plibrary_extensions_diff_only
     ./gradlew library-extensions-drag:bintrayUpload -x test -Plibrary_extensions_drag_only
     ./gradlew library-extensions-expandable:bintrayUpload -x test -Plibrary_extensions_expandable_only
