ffb03a0d71e6d99253f9ce6fddd9f36c5b408678
Mike Penz <mikepenz@gmail.com>
2019-02-10 10:46:07 +0100

    * convert more sample activities to kotlin

---
+++ app/src/main/java/com/mikepenz/fastadapter/app/RadioButtonSampleActivity.kt
@@ -0,0 +1,112 @@
+package com.mikepenz.fastadapter.app
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mikepenz.fastadapter.IAdapter
+import com.mikepenz.fastadapter.adapters.FastItemAdapter
+import com.mikepenz.fastadapter.app.items.RadioButtonSampleItem
+import com.mikepenz.fastadapter.listeners.OnClickListener
+import com.mikepenz.fastadapter.select.SelectExtension
+import com.mikepenz.materialize.MaterializeBuilder
+import kotlinx.android.synthetic.main.activity_sample.*
+import java.util.*
+
+class RadioButtonSampleActivity : AppCompatActivity() {
+
+    //save our FastAdapter
+    private lateinit var fastItemAdapter: FastItemAdapter<RadioButtonSampleItem>
+    private lateinit var selectExtension: SelectExtension<RadioButtonSampleItem>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        findViewById<View>(android.R.id.content).systemUiVisibility = findViewById<View>(android.R.id.content).systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample)
+
+        // Handle Toolbar
+        setSupportActionBar(toolbar)
+
+        //style our ui
+        MaterializeBuilder().withActivity(this).build()
+
+        //create our FastAdapter which will manage everything
+        fastItemAdapter = FastItemAdapter()
+        selectExtension = SelectExtension(fastItemAdapter)
+        fastItemAdapter.addExtension(selectExtension)
+        selectExtension.isSelectable = true
+
+        //configure our fastAdapter
+        fastItemAdapter.onClickListener = object : OnClickListener<RadioButtonSampleItem> {
+            override fun onClick(v: View?, adapter: IAdapter<RadioButtonSampleItem>, item: RadioButtonSampleItem, position: Int): Boolean {
+                v?.let {
+                    Toast.makeText(v.context, item.name?.getText(v.context), Toast.LENGTH_LONG).show()
+                }
+                return false
+            }
+
+        }
+        fastItemAdapter.onPreClickListener = object : OnClickListener<RadioButtonSampleItem> {
+            override fun onClick(v: View?, adapter: IAdapter<RadioButtonSampleItem>, item: RadioButtonSampleItem, position: Int): Boolean {
+                // consume otherwise radio/checkbox will be deselected
+                return true
+            }
+        }
+
+        fastItemAdapter.addEventHook(RadioButtonSampleItem.RadioButtonClickEvent())
+
+        //get our recyclerView and do basic setup
+        val recyclerView = findViewById<RecyclerView>(R.id.rv)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.itemAnimator = DefaultItemAnimator()
+        recyclerView.adapter = fastItemAdapter
+
+        //fill with some sample data
+        var x = 0
+        val items = ArrayList<RadioButtonSampleItem>()
+        for (s in ALPHABET) {
+            val count = Random().nextInt(20)
+            for (i in 1..count) {
+                val item = RadioButtonSampleItem().withName("$s Test $x")
+                item.identifier = (100 + x).toLong()
+                items.add(item)
+                x++
+            }
+        }
+        fastItemAdapter.add(items)
+
+        //restore selections (this has to be done after the items were added
+        fastItemAdapter.withSavedInstanceState(savedInstanceState)
+
+        //set the back arrow in the toolbar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(false)
+    }
+
+    override fun onSaveInstanceState(_outState: Bundle?) {
+        var outState = _outState
+        //add the values which need to be saved from the adapter to the bundle
+        outState = fastItemAdapter.saveInstanceState(outState)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //handle the click on the back arrow click
+        return when (item.itemId) {
+            android.R.id.home -> {
+                Toast.makeText(applicationContext, "selections = " + selectExtension.selections, Toast.LENGTH_LONG).show()
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    companion object {
+        private val ALPHABET = arrayOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+    }
+}
