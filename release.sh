e402205e9f39ee58b024192873b396bdd8c40886
Mike Penz <mikepenz@gmail.com>
2020-01-30 16:52:28 +0100

    - introduce new binding module which adds abstract items to simplify usage with viewbinding / databinding - update to AS 4.0.0-alpha09 for viewbinding / databinding - add compileOnly dependency for viewBinding interface - use iconItem from viewBinding in the sample app - FIX / REGARDING https://github.com/mikepenz/FastAdapter/issues/822

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@
 if [ "$1" = "release" ];
 then
     ./gradlew library-core:bintrayUpload -Plibrary_core_only
+    ./gradlew library-extensions-binding:bintrayUpload -x test -x lint -Plibrary_extensions_binding_only
     ./gradlew library-extensions-diff:bintrayUpload -x test -x lint -Plibrary_extensions_diff_only
     ./gradlew library-extensions-drag:bintrayUpload -x test -x lint -Plibrary_extensions_drag_only
     ./gradlew library-extensions-expandable:bintrayUpload -x test -x lint -Plibrary_extensions_expandable_only
