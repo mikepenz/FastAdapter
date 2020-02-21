c1037ceefdcdcd681fedaff2fddd55ecb382beb6
Mike Penz <mikepenz@gmail.com>
2020-02-21 18:39:03 +0100

    - even further simplify the view binding case, by providing an AbstractItem.kt which does not require a custom ViewHolder - introduce extension functions to simplify hooking up click event hooks to binding items - FIX https://github.com/mikepenz/FastAdapter/issues/872 - simplify AbstractItem.kt by basing on top of the BaseItem.kt - Showcasing simplified item in sample

--- app/src/main/java/com/mikepenz/fastadapter/app/binding/BindingIconItem.kt
+++ app/src/main/java/com/mikepenz/fastadapter/app/binding/BindingIconItem.kt
@@ -4,14 +4,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.mikepenz.aboutlibraries.util.getThemeColor
 import com.mikepenz.fastadapter.IExpandable
-import com.mikepenz.fastadapter.IItemVHFactory
 import com.mikepenz.fastadapter.IParentItem
 import com.mikepenz.fastadapter.ISubItem
 import com.mikepenz.fastadapter.app.R
 import com.mikepenz.fastadapter.app.databinding.IconItemBinding
-import com.mikepenz.fastadapter.binding.BaseBindingItemVHFactory
+import com.mikepenz.fastadapter.binding.AbstractBindingItem
 import com.mikepenz.fastadapter.binding.BindingViewHolder
-import com.mikepenz.fastadapter.items.BaseItem
 import com.mikepenz.iconics.IconicsDrawable
 import com.mikepenz.iconics.typeface.IIcon
 import com.mikepenz.iconics.utils.colorInt
@@ -19,10 +17,7 @@ import com.mikepenz.iconics.utils.colorInt
 /**
  * Created by mikepenz on 28.12.15.
  */
-class BindingIconItem : BaseItem<BindingViewHolder<IconItemBinding>>(), IExpandable<BindingViewHolder<IconItemBinding>> {
-
-    override val factory: IItemVHFactory<BindingViewHolder<IconItemBinding>>? = BindingIconFactory
-
+class BindingIconItem : AbstractBindingItem<IconItemBinding>(), IExpandable<BindingViewHolder<IconItemBinding>> {
     var icon: IIcon? = null
 
     override var parent: IParentItem<*>? = null
@@ -57,33 +52,22 @@ class BindingIconItem : BaseItem<BindingViewHolder<IconItemBinding>>(), IExpanda
 
     /**
      * binds the data of this item onto the viewHolder
-     *
-     * @param holder the viewHolder of this item
      */
-    override fun bindView(holder: BindingViewHolder<IconItemBinding>, payloads: MutableList<Any>) {
-        super.bindView(holder, payloads)
-
+    override fun bindView(binding: IconItemBinding, payloads: MutableList<Any>) {
         //define our data for the view
         icon?.let {
-            holder.binding.icon.icon = IconicsDrawable(holder.binding.icon.context, it).apply {
-                colorInt = holder.binding.root.context.getThemeColor(R.attr.colorOnSurface)
+            binding.icon.icon = IconicsDrawable(binding.icon.context, it).apply {
+                colorInt = binding.root.context.getThemeColor(R.attr.colorOnSurface)
             }
         }
-        holder.binding.name.text = icon?.name
+        binding.name.text = icon?.name
     }
 
-    override fun unbindView(holder: BindingViewHolder<IconItemBinding>) {
-        super.unbindView(holder)
-        holder.binding.icon.setImageDrawable(null)
+    override fun unbindView(binding: IconItemBinding) {
+        binding.icon.setImageDrawable(null)
     }
-}
 
-object BindingIconFactory : BaseBindingItemVHFactory<IconItemBinding, BindingViewHolder<IconItemBinding>>() {
     override fun createBinding(inflater: LayoutInflater, parent: ViewGroup?): IconItemBinding {
         return IconItemBinding.inflate(inflater, parent, false)
-    }
-
-    override fun getViewHolder(viewBinding: IconItemBinding): BindingViewHolder<IconItemBinding> {
-        return BindingViewHolder(viewBinding)
     }
 }
