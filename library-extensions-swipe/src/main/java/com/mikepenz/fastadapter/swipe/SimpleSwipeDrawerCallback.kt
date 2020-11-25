de273123dd8b2926ad50262142b28a15e41ea080
Robb <zeugmaonline@hotmail.com>
2020-11-25 22:07:34 +0100

    Fix broken swipe distance when item width is smaller than the recyclerView's width (e.g. grid layout)

--- library-extensions-swipe/src/main/java/com/mikepenz/fastadapter/swipe/SimpleSwipeDrawerCallback.kt
+++ library-extensions-swipe/src/main/java/com/mikepenz/fastadapter/swipe/SimpleSwipeDrawerCallback.kt
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.IItem
+import kotlin.math.max
+import kotlin.math.min
 
 
 /**
@@ -153,7 +155,11 @@ class SimpleSwipeDrawerCallback @JvmOverloads constructor(private val swipeDirs:
             var swipeableView = itemView
             if (viewHolder is IDrawerSwipeableViewHolder) swipeableView = viewHolder.swipeableView
 
-            swipeableView.translationX = dX * swipeWidthPc
+            // Android's ItemTouchHelper incorrectly sets dX to recyclerView.width when swiping to the max,
+            // which breaks the animation when itemView is smaller than that (e.g. two columns layout)
+            // => fix animation by limiting dX to the itemView's width
+            val realDx = if (dX < 0) max(dX, -itemView.width * 1f) else min(dX, itemView.width * 1f)
+            swipeableView.translationX = realDx * swipeWidthPc
         } else super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
     }
 
