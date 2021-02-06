44273986115bc30af6ac80cc33a892d6df976afa
Mike Penz <mikepenz@gmail.com>
2021-02-06 11:15:08 +0100

    - rename modules to match artifact ids - update ci pipeline to release to sonatype

---
+++ fastadapter-extensions-expandable/src/main/java/com/mikepenz/fastadapter/expandable/items/AbstractExpandableItem.kt
@@ -0,0 +1,23 @@
+package com.mikepenz.fastadapter.expandable.items
+
+import androidx.recyclerview.widget.RecyclerView
+import com.mikepenz.fastadapter.*
+import com.mikepenz.fastadapter.items.AbstractItem
+
+abstract class AbstractExpandableItem<VH : RecyclerView.ViewHolder> :
+        AbstractItem<VH>(),
+        IItem<VH>,
+        IExpandable<VH> {
+    private val _subItems = MutableSubItemList<ISubItem<*>>(this)
+
+    override var isExpanded: Boolean = false
+    override var parent: IParentItem<*>? = null
+    override var subItems: MutableList<ISubItem<*>>
+        set(value) = _subItems.setNewList(value)
+        get() = _subItems
+
+    override val isAutoExpanding: Boolean = true
+    override var isSelectable: Boolean
+        get() = subItems.isEmpty()
+        set(_) {}
+}
