2c6e43ef216b7813ad370c510992c2915cc45f7c
Mike Penz <mikepenz@gmail.com>
2021-09-10 11:36:01 +0200

    - add javadoc

--- fastadapter-extensions-swipe/src/main/java/com/mikepenz/fastadapter/swipe/Extensions.kt
+++ fastadapter-extensions-swipe/src/main/java/com/mikepenz/fastadapter/swipe/Extensions.kt
@@ -2,13 +2,16 @@ package com.mikepenz.fastadapter.swipe
 
 import androidx.recyclerview.widget.ItemTouchHelper
 
+/**
+ * Retrieve the swipe directions allowed for this [ISwipeable] element.
+ */
 internal fun ISwipeable.getSwipeDirs(dirs: Int): Int {
-    var dirs = dirs
+    var directions = dirs
     if (!isDirectionSupported(ItemTouchHelper.LEFT)) {
-        dirs = dirs and ItemTouchHelper.LEFT.inv()
+        directions = dirs and ItemTouchHelper.LEFT.inv()
     }
     if (!isDirectionSupported(ItemTouchHelper.RIGHT)) {
-        dirs = dirs and ItemTouchHelper.RIGHT.inv()
+        directions = dirs and ItemTouchHelper.RIGHT.inv()
     }
-    return dirs
+    return directions
 }
