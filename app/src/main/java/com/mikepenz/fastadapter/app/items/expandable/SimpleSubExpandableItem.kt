506a0b9a40ac7d66b3249baf04ecc4fe979b3877
Cees Wiersma <ceeswiersma@hotmail.com>
2021-11-25 15:08:48 +0100

    Remove obsolete imports

--- app/src/main/java/com/mikepenz/fastadapter/app/items/expandable/SimpleSubExpandableItem.kt
+++ app/src/main/java/com/mikepenz/fastadapter/app/items/expandable/SimpleSubExpandableItem.kt
@@ -1,18 +1,12 @@
 package com.mikepenz.fastadapter.app.items.expandable
 
-import android.animation.ArgbEvaluator
-import android.animation.ValueAnimator
 import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.StringRes
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.mikepenz.fastadapter.ClickListener
-import com.mikepenz.fastadapter.IAdapter
-import com.mikepenz.fastadapter.IClickable
 import com.mikepenz.fastadapter.ISubItem
 import com.mikepenz.fastadapter.app.R
 import com.mikepenz.fastadapter.expandable.ExpandableExtension
